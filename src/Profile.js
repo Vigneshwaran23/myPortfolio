c7cc8b64db0ef2d13a4028ba649d57382855c12e
Vigneshwaran23 <60375620+Vigneshwaran23@users.noreply.github.com>
2025-01-04 19:38:49 +0530

    Profile Update

--- src/Profile.js
+++ src/Profile.js
@@ -4,7 +4,7 @@ import './Profile.css';
 const Profile = () => (
   <section id="profile">
     <h2>Profile</h2>
-    <p>I am a seasoned UI/UX developer with over 8 years of experience in front-end development using Angular, React, JavaScript, jQuery, HTML, and CSS/LESS. I excel in developing creative and effective web applications, including debugging frontend and indexing issues. Proficient in Java and related technologies, I have a solid grasp of AWS server logs and deployment processes through Jenkins. My expertise extends to SDLC, Agile methodology, AEM, Endeca, and basic shell scripting.
+    <p>I am a seasoned UI/UX developer with over 9 years of experience in front-end development using Angular, React, JavaScript, jQuery, HTML, and CSS/LESS. I excel in developing creative and effective web applications, including debugging frontend and indexing issues. Proficient in Java and related technologies, I have a solid grasp of AWS server logs and deployment processes through Jenkins. My expertise extends to SDLC, Agile methodology, AEM, Endeca, and basic shell scripting.
 
 I bring a strong work ethic, a desire to succeed, and a dedication to contributing significantly to any organization. Skilled in cross-browser compatibility, I am adept with Git and JIRA, and possess a fundamental understanding of Dynatrace. Comfortable in Windows, Unix, and Linux environments, I also have extensive experience with IDEs like Eclipse and Netbeans, and adhere to coding standards and best practices.</p>
   </section>
